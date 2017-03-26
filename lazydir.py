254bddf5dad4cc0fb018b2508cd474fe3c23a302
Alexandros Peitsinis <alexpeitsinis@gmail.com>
2017-03-27 01:36:45 +0300

    removed TODOS in main module

--- lazydir.py
+++ lazydir.py
@@ -4,13 +4,6 @@ lazydir
 
 Simple directory structure validation and creation.
 
-TODO:
-    - implementation
-    - tests
-    - cli interface
-    - conf types
-    - usage docs
-
 """
 
 from __future__ import print_function
